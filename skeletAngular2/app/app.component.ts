d76b2b0158c20715bba12b18b51e03d53597301b
schuylermk <schuyler.m.k@gmail.com>
2016-03-24 10:34:07 -0700

    Adds a minimal Angular2 template

---
+++ skeletAngular2/app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Second Angular 2 App</h1><div class="tpop"><p></p></div><div class="tpop"></div><div class="tpop"><p></p></div>'
+})
+export class AppComponent { }
